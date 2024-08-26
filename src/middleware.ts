975bf886a5e36f8a039ef671f2fcedcb68af160d
SenhuiZhao <zhaosky12187@gmail.com>
2024-08-25 21:38:51 -0400

    Authorization Admin

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
+
+export async function middleware ( req: NextRequest){
+
+    if(await isAuthenticated(req)===false){
+        return new NextResponse("Unauthorized", {status: 401, headers: {"WWW-Authenticate" : "Basic"}})
+    }
+}
+
+async function isAuthenticated(req: NextRequest) {
+    const authHeader = req.headers.get("authorization") || req.headers.get("Authorization")
+
+    if (authHeader == null){
+        return false
+    }
+
+    const [userName, password] = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":")
+    
+    return userName === process.env.ADMIN_USERNAME && (await isValidPassword(password, process.env.ADMIN_HASHED_PASSWORD as string))
+    
+    // isValidPassword(password, "123456")
+    // return Promise.resolve(false)
+
+}
+
+export const config = {
+    matcher: "/admin/:path*"
+}
